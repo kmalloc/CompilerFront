3bf928c0175d81a7f336e059dfb2d2e105f38c70
Mingdong Liao <kmalloc@live.com>
2015-10-23 04:36:16 -0400

    format tab & space

--- ink/Variant.h
+++ ink/Variant.h
@@ -11,134 +11,131 @@
 
 namespace VariantHelper {
 
-// check if a type exists in the variadic type list
-template <typename T, typename ...TS> struct TypeExist;
+    // check if a type exists in the variadic type list
+    template <typename T, typename ...TS> struct TypeExist;
 
-template <typename T>
-struct TypeExist<T>
-{
-    enum { exist = 0 };
-    static constexpr std::size_t id = 0;
-};
-
-template <typename T, typename T2, typename ...TS>
-struct TypeExist<T, T2, TS...>
-{
-    enum { exist = std::is_same<T, T2>::value || TypeExist<T, TS...>::exist };
-    static constexpr std::size_t id = std::is_same<T, T2>::value? 1 : 1 + TypeExist<T, TS...>::id;
-};
-
-
-// extract type from the variadic type list at position k
-template <std::size_t k, typename ...TS> struct ExtractType;
-
-template <typename T, typename ...TS>
-struct ExtractType<0, T, TS...>
-{
-};
-
-template <typename T, typename ...TS>
-struct ExtractType<1, T, TS...>
-{
-    using type = T;
-};
-
-template <std::size_t k, typename T, typename ...TS>
-struct ExtractType<k, T, TS...>
-{
-    using type = typename ExtractType<k - 1, TS...>::type;
-};
-
-
-// get the max size of type in the type list
-template <typename ...TS> struct TypeMaxSize;
-
-template <>
-struct TypeMaxSize<>
-{
-    static constexpr std::size_t value = 0;
-};
-
-template <typename T, typename ...TS>
-struct TypeMaxSize<T, TS...>
-{
-    static constexpr std::size_t cur = alignof(T);
-    static constexpr std::size_t next = TypeMaxSize<TS...>::value;
-    static constexpr std::size_t value = cur > next? cur : next;
-};
-
-
-template <typename ...TS> struct TryRelease
-{
-    static void Destroy(unsigned char* p, std::size_t id) {}
-};
-
-template <typename T, typename ...TS>
-struct TryRelease<T, TS...>
-{
-    static void Destroy(unsigned char* p, std::size_t id)
+    template <typename T>
+    struct TypeExist<T>
     {
-        if (id == 0) return;
+        enum { exist = 0 };
+        static constexpr std::size_t id = 0;
+    };
 
-        if (id > 1)
-        {
-            TryRelease<TS...>::Destroy(p, id - 1);
-        }
-        else
-        {
-            reinterpret_cast<T*>(p)->~T();
-        }
-    }
-};
-
-template <typename ...TS>
-struct CopyConstruct
-{
-    static void Copy(const unsigned char*, unsigned char*, std::size_t) {}
-};
-
-template <typename T, typename ...TS>
-struct CopyConstruct<T, TS...>
-{
-    static void Copy(const unsigned char* f, unsigned char* p, std::size_t id)
+    template <typename T, typename T2, typename ...TS>
+    struct TypeExist<T, T2, TS...>
     {
-        if (id == 0) return;
+        enum { exist = std::is_same<T, T2>::value || TypeExist<T, TS...>::exist };
+        static constexpr std::size_t id = std::is_same<T, T2>::value? 1 : 1 + TypeExist<T, TS...>::id;
+    };
 
-        if (id > 1)
-        {
-            CopyConstruct<TS...>::Copy(f, p, id - 1);
-        }
-        else
-        {
-            new(p) T(*reinterpret_cast<const T*>(f));
-        }
-    }
-};
+    // extract type from the variadic type list at position k
+    template <std::size_t k, typename ...TS> struct ExtractType;
 
-template <typename ...TS>
-struct MoveConstruct
-{
-    static void Move(unsigned char* f, unsigned char* p, std::size_t id) {}
-};
-
-template <typename T, typename ...TS>
-struct MoveConstruct<T, TS...>
-{
-    static void Move(unsigned char* f, unsigned char* p, std::size_t id)
+    template <typename T, typename ...TS>
+    struct ExtractType<0, T, TS...>
     {
-        if (id == 0) return;
+    };
 
-        if (id > 1)
+    template <typename T, typename ...TS>
+    struct ExtractType<1, T, TS...>
+    {
+        using type = T;
+    };
+
+    template <std::size_t k, typename T, typename ...TS>
+    struct ExtractType<k, T, TS...>
+    {
+        using type = typename ExtractType<k - 1, TS...>::type;
+    };
+
+    // get the max size of type in the type list
+    template <typename ...TS> struct TypeMaxSize;
+
+    template <>
+    struct TypeMaxSize<>
+    {
+        static constexpr std::size_t value = 0;
+    };
+
+    template <typename T, typename ...TS>
+    struct TypeMaxSize<T, TS...>
+    {
+        static constexpr std::size_t cur = alignof(T);
+        static constexpr std::size_t next = TypeMaxSize<TS...>::value;
+        static constexpr std::size_t value = cur > next? cur : next;
+    };
+
+    template <typename ...TS> struct TryRelease
+    {
+        static void Destroy(unsigned char* p, std::size_t id) {}
+    };
+
+    template <typename T, typename ...TS>
+    struct TryRelease<T, TS...>
+    {
+        static void Destroy(unsigned char* p, std::size_t id)
         {
-            MoveConstruct<TS...>::Move(f, p, id - 1);
+            if (id == 0) return;
+
+            if (id > 1)
+            {
+                TryRelease<TS...>::Destroy(p, id - 1);
+            }
+            else
+            {
+                reinterpret_cast<T*>(p)->~T();
+            }
         }
-        else
+    };
+
+    template <typename ...TS>
+    struct CopyConstruct
+    {
+        static void Copy(const unsigned char*, unsigned char*, std::size_t) {}
+    };
+
+    template <typename T, typename ...TS>
+    struct CopyConstruct<T, TS...>
+    {
+        static void Copy(const unsigned char* f, unsigned char* p, std::size_t id)
         {
-            T* fp = reinterpret_cast<T*>(f);
-            new(p) T(std::move(*fp));
+            if (id == 0) return;
+
+            if (id > 1)
+            {
+                CopyConstruct<TS...>::Copy(f, p, id - 1);
+            }
+            else
+            {
+                new(p) T(*reinterpret_cast<const T*>(f));
+            }
         }
-    }
-};
+    };
+
+    template <typename ...TS>
+    struct MoveConstruct
+    {
+        static void Move(unsigned char* f, unsigned char* p, std::size_t id) {}
+    };
+
+    template <typename T, typename ...TS>
+    struct MoveConstruct<T, TS...>
+    {
+        static void Move(unsigned char* f, unsigned char* p, std::size_t id)
+        {
+            if (id == 0) return;
+
+            if (id > 1)
+            {
+                MoveConstruct<TS...>::Move(f, p, id - 1);
+            }
+            else
+            {
+                T* fp = reinterpret_cast<T*>(f);
+                new(p) T(std::move(*fp));
+            }
+        }
+    };
 
 } // end namespace VariantHelper
 
@@ -159,6 +156,7 @@ public:
     Variant(T&& v)
         : type_(VariantHelper::TypeExist<T, TS...>::id)
     {
+        // following is a little overkill maybe.
         static_assert(VariantHelper::TypeExist<T, TS...>::exist, "invalid type for invariant.");
 
         new(data_) T(std::forward<T>(v));
@@ -266,7 +264,6 @@ private:
         VariantHelper::TryRelease<TS...>::Destroy(data_, type_);
         type_ = 0;
     }
-
 
 private:
     std::size_t type_;
